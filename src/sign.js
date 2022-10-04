5c8d3655265346fed5b0a0f78c36c2c763e1ea5d
VladislavGazuk <vladislav.gazuk@gmail.com>
2022-10-04 15:46:53 +0200

    add sign.html, add sign.js, modified some files

--- src/sign.js
+++ src/sign.js
@@ -1,30 +1,24 @@
-const iconInfo = document.querySelector('.icon');
+const btnSign = document.querySelector('.btn-sign');
 
-iconInfo.addEventListener('mouseover', () => {
-    document.querySelector('.info').hidden = false;
-});
-
-iconInfo.addEventListener('mouseout', () => {
-    document.querySelector('.info').hidden = true;
-});
-
-const btnLogin = document.querySelector('.btn-login');
-
-btnLogin.addEventListener('click', () => {
+btnSign.addEventListener('click', () => {
     const emailPhone = document.querySelector('.email-phone');
     const password = document.querySelector('.password');
+    const confirmPassword = document.querySelector('.confirm-password');
     try {
         if (!/^([a-zA-Z0-9\.\_\-]+@[a-z]+\.[a-z]{1,3})|(\+[0-9]{12})$/g.test(emailPhone.value)) throw new Error('Wrong email or phone number format');
         if (!/^\w{8,}$/g.test(password.value)) throw new Error('Password entered too short');
+        if (password.value !== confirmPassword.value) throw new Error('Passwords do not match');
 
-        alert('You have successfully authorized in the system');
+        alert('You have successfully registered in the system');
 
         emailPhone.value = '';
         password.value = '';
+        confirmPassword.value = '';
     } catch (error) {
         alert(error.message);
 
         emailPhone.value = '';
         password.value = '';
+        confirmPassword.value = '';
     }
 });
